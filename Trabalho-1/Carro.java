afd8188afa08de3781d270ce5652b0d4afb6225b
gabrielgusn <gabriel.gnicolodi@gmail.com>
2022-09-29 14:25:22 -0300

    atualizado carro e simulador

--- Trabalho-1/Carro.java
+++ Trabalho-1/Carro.java
@@ -1,6 +1,19 @@
-public class Carro {
-    private Double combustivel;
-    private Roda[] roda = new Roda[4];
+import java.util.Random;
 
+public class Carro {
+    private int id;
+    private Roda[] roda = new Roda[4];
+    private Double combustivel;
+    private Double precoVenda;
+    private boolean ipvaPago;
+
+    public Carro(){}
+    
+    public Carro(int id){
+        Random r = new Random();
+    
+        this.ipvaPago = r.nextBoolean();
+        
+    }
 
 }
---
+++ Trabalho-1/Simulador.java
@@ -0,0 +1,8 @@
+public class Simulador {
+    private static int qtiDeVeiculos;
+    private static Carro[] veiculos = new Carro[20];
+
+    public void incluirVeiculo(int id){
+        
+    }
+}
